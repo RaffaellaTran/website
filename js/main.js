68cbc8aa4bc65c4295a4d7ad293afbe32dc2d278
RaffaellaTran <raffaella.tran@gmail.com>
2018-03-25 12:08:24 +0300

    insert loaded

--- js/main.js
+++ js/main.js
@@ -19,3 +19,11 @@ $('.navbar-collapse a').click(function(){
 	$(".navbar-collapse").collapse('hide');
 });
 
+$(document).ready(function() {
+	
+	setTimeout(function(){
+		$('body').addClass('loaded');
+		$('h1').css('color','#222222');
+	}, 3000);
+	
+});
