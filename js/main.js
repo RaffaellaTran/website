ed69a0aabc09f2093d7e65a77ee094760493d00d
RaffaellaTran <raffaella.tran@gmail.com>
2018-03-25 14:38:35 +0300

    fix errors

--- js/main.js
+++ js/main.js
@@ -15,10 +15,7 @@ function topFunction() {
 	document.documentElement.scrollTop = 0;
 }
 
-$('.navbar-collapse a').click(function(){
-	$(".navbar-collapse").collapse('hide');
-});
-
+//loader
 $(document).ready(function() {
 	
 	setTimeout(function(){
@@ -26,4 +23,49 @@ $(document).ready(function() {
 		$('h1').css('color','#222222');
 	}, 3000);
 	
-});
+})
+
+//intersectionObserver
+
+ let visiblity = 'invisible';
+    let statusText = 'No box';
+    // Create new IntersectionObserver
+    const io = new IntersectionObserver(entries => {
+      
+      // Available data when an intersection happens
+      console.log(entries);
+      // Element enters the viewport
+      if(entries[0].intersectionRatio !== 0) {
+        visiblity = 'visible';
+        statusText = entries[0].target.textContent;
+      // Element leaves the viewport 
+      } else {
+        visiblity = 'invisible';
+        statusText = '';
+      }
+      updateStatus(visiblity, statusText);
+    });
+    
+    // Elements to be observed
+    const box1 = document.querySelector('.box1');
+    const box2 = document.querySelector('.box2');
+    const box3 = document.querySelector('.box3');
+    const box4 = document.querySelector('.box4');
+    const box5 = document.querySelector('.box5');
+    const box6 = document.querySelector('.box6');
+    
+    // Start observing .box
+    io.observe(box1);
+    io.observe(box2);
+    io.observe(box3);
+     io.observe(box4);
+    io.observe(box5);
+    io.observe(box6);
+    // Just necessary for displaying the current status
+    function updateStatus(visiblity, statusText) {
+      console.log(visiblity);
+      const status = document.querySelector('.status');
+    //  console.log(statusText);
+    //  status.textContent = statusText;
+    //  status.className = 'status status--' + visiblity;
+    }
